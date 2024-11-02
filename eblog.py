4d351a11d666e6e0ca56adecbdd954378d19b63b
buttfa <1662332017@qq.com>
2024-11-02 10:25:52 +0800

    docs: Add comments in eblog.py.

--- eblog.py
+++ eblog.py
@@ -1,12 +1,14 @@
-# Used here to determine whether the virtual environment folder exists
-import os
-# Used here to execute shell commands
-import subprocess
-# Used here to obtain command-line parameters
-import sys
+import os           # Use 'os' to determine whether the virtual environment folder exists
+import subprocess   # Use 'subprocess' to execute shell commands
+import sys          # Use 'sys' to obtain command-line parameters
 
+
+# The path of the python virtual environment folder.
 venv_folder_path = 'venv'
+
 def build_environment():
+    """Build the python virtual environment.
+    """
     # Check if the python virtual environment exists.
     if os.path.exists(venv_folder_path) and os.path.isdir(venv_folder_path):
         print("[Easy-Blog]: The virtual environment already exists.")
@@ -17,25 +19,38 @@ def build_environment():
     subprocess.run(f"source {venv_folder_path}/bin/activate && pip install -r requirements.txt && deactivate", shell=True)
 
 def destroy_environment():
+    """Destroy the python virtual environment.
+    """
     subprocess.run(f"rm -rf {venv_folder_path}", shell=True)
 
 def run_blog():
+    """Run the blog.
+    """
     subprocess.run(f"source {venv_folder_path}/bin/activate && export FLASK_APP=app && export FLASK_ENV=development && flask run && deactivate", shell=True)
 
 def main():
-
-    if len(sys.argv) > 1:
+    """The main function.
+    """
+    # Check if the command is valid.
+    if len(sys.argv) > 2:
         print("[Easy-Blog]: Invalid command.")
         return
     
-    if sys.argv[0] == "build":
+    # If the command is build, build the virtual environment.
+    if sys.argv[1] == "build":
         print("[Easy-Blog]: Building the virtual environment...")
         build_environment()
-    elif sys.argv[0] == "destroy":
+        print("[Easy-Blog]: Virtual environment built successfully.")
+    # If the command is destroy, destroy the virtual environment.
+    elif sys.argv[1] == "destrory":
         print("[Easy-Blog]: Destroying the virtual environment...")
         destroy_environment()
-    elif sys.argv[0] == "run":
+        print("[Easy-Blog]: Virtual environment destroyed successfully.")
+    # If the command is run, run the blog.
+    elif sys.argv[1] == "run":
         print("[Easy-Blog]: Running the blog...")
+        run_blog()
+    # If the command is invalid, print an error message.
     else:
         print("[Easy-Blog]: Invalid command.")
 
