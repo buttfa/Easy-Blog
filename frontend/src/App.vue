92f35f3c130568d2ba5ccbfb2379e66e165eef24
buttfa <1662332017@qq.com>
2024-11-21 09:48:04 +0800

    feat: Introduce Axios in the frontend for interaction with the backend.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,28 @@
 <template>
+    <button @click="getPosts">Get Posts</button>
     <p> Hello Easy-Blog! </p>
+    <li v-for="post in post_list" :key="post['id']">{{ post['title'] }} {{ post['author'] }} {{ post['content'] }}</li>
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            msg: 'Hello Easy-Blog!',
+            post_list: []       
+        }
+    },
+
+    methods: {
+        getPosts() {
+            axios.get('http://127.0.0.1:5000/get_post_list')
+            .then(res => {
+                this.post_list = res.data['post_list']
+                console.log(res.data['post_list'])
+            })
+        }
+    }
+}
 </script>
