a461c2ab513723067143de70e0b3af99c8b19790
buttfa <1662332017@qq.com>
2025-01-09 10:40:02 +0800

    refactor: Split the run_blog function from the eblog.py script.

--- eblog.py
+++ eblog.py
@@ -1,15 +1,13 @@
 import os  # Use 'os' to determine whether the virtual environment folder exists
-import subprocess  # Use 'subprocess' to execute shell commands
 import sys  # Use 'sys' to obtain command-line parameters
-import threading
+import subprocess  # Use 'subprocess' to execute shell commands
 from sqlite3 import connect
 from getpass import getpass
 from hashlib import sha256
 
-# The path of the python virtual environment folder.
-venv_folder_path = "backend/venv"
-# The path of the database folder.
-db_folder_path = "backend/db"
+from script.run import run_blog
+from script.config import venv_folder_path, db_folder_path
+
 # system_de
 system_dependencies = {
     "centos": {
@@ -220,85 +218,6 @@ def destroy_environment() -> bool:
     return True
 
 
-def replace_line_with_prefix(file_path, old_prefix, new_line):
-    with open(file_path, "r", encoding="utf-8") as file:
-        lines = file.readlines()
-
-    modified_lines = []
-    for line in lines:
-        if line.startswith(old_prefix):
-            modified_lines.append(new_line + "\n")
-        else:
-            modified_lines.append(line)
-
-    with open(file_path, "w", encoding="utf-8") as file:
-        file.writelines(modified_lines)
-
-
-def configure_proxy(api_url):
-    replace_line_with_prefix(
-        "frontend/vite.config.js", "        target", f"        target: '{api_url}',"
-    )
-
-
-def run_frontend():
-    """Run the frontend."""
-
-    subprocess.run(
-        f"cd frontend && npx vite --host",
-        shell=True,
-    )
-
-
-def run_backend():
-    """Run the backend."""
-    subprocess.run(
-        f"bash -c 'source {venv_folder_path}/bin/activate && export FLASK_APP=backend/blog && export FLASK_ENV=development && flask run --debug --host='0.0.0.0' && deactivate'",
-        shell=True,
-    )
-
-
-def run_blog(pos: str, mode: str) -> bool:
-    """Run the blog.
-
-    Returns:
-        bool: Return False when fail to run the blog.
-    """
-
-    # If running locally, configure the proxy IP to 127.0.0.1.
-    if pos == "local":
-        configure_proxy("http://127.0.0.1:5000")
-
-    # If it is running remotely, configure the proxy IP as a public IP.
-    elif pos == "remote":
-        process = subprocess.Popen(
-            "curl ifconfig.me",
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            shell=True,
-        )
-        server_ip, stderr = process.communicate()
-
-        with open("frontend/.env", "w") as env_dev:
-            env_dev.write(f"VITE_EBLOG_API_URL=/api")
-
-        configure_proxy(f"http://{server_ip}:5000")
-
-    else:
-        print("[Easy-Blog]: The position is not supported.")
-        return False
-
-    thread_frontend = threading.Thread(target=run_frontend)
-    thread_backend = threading.Thread(target=run_backend)
-
-    thread_frontend.start()
-    thread_backend.start()
-
-    thread_frontend.join()
-    thread_backend.join()
-
-    return True
 
 
 def main():
---
+++ script/config.py
@@ -0,0 +1,4 @@
+# The path of the python virtual environment folder.
+venv_folder_path = "backend/venv"
+# The path of the database folder.
+db_folder_path = "backend/db"
